3a70fcc843adcf15cc68a840c8f602c8d6939c37
Aleix Suriñach Cobos <114676817+AleixSuri@users.noreply.github.com>
2024-11-28 10:56:23 +0100

    Uploaded files

---
+++ p3-aUEBs.h
@@ -0,0 +1,29 @@
+/**************************************************************************/
+/*                                                                        */
+/* L'aplicació UEB amb sockets TCP/IP                                     */
+/* Fitxer capçalera de aUEBs.c                                            */
+/*                                                                        */
+/* Autors: Arnau Herrera i Aleix Suriñach                                 */
+/* Data: novembre 2024                                                    */
+/*                                                                        */
+/**************************************************************************/
+
+/* Declaració de funcions EXTERNES de aUEBs.c, és a dir, d'aquelles       */
+/* funcions que es faran servir en un altre fitxer extern a aUEBs.c,      */
+/* p.e., int UEBs_FuncioExterna(arg1, arg2...) { }                        */
+/* El fitxer extern farà un #include del fitxer aUEBs.h a l'inici, i      */
+/* així les funcions seran conegudes en ell.                              */
+/* En termes de capes de l'aplicació, aquest conjunt de funcions          */
+/* EXTERNES formen la interfície de la capa UEB, en la part servidora.    */
+
+int UEBs_IniciaServ(int *SckEsc, int portTCPser, char *TextRes);
+int UEBs_AcceptaConnexio(int SckEsc, char *TextRes);
+int UEBs_ServeixPeticio(int SckCon, char *TipusPeticio, char *NomFitx, char *TextRes, char *TextTemps);
+int UEBs_TancaConnexio(int SckCon, char *TextRes);
+int UEBs_TrobaAdrSckConnexio(int SckCon, char *IPloc, int *portTCPloc, char *IPrem, int *portTCPrem, char *TextRes);
+int UEBs_HaArribatAlgunaCosaPerLlegir(const int *LlistaSck, int LongLlistaSck, char *TextRes);
+/* int UEBs_FuncioExterna(arg1, arg2...);                                 */
+
+int compara_vectors(const char *vec1, const char *vec2, int mida);
+
+
